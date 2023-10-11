a0df8bd11b82409e1935d577b4322b9039f12a20
Artur Kowalski <artur.kowalski@3mdeb.com>
2023-10-11 19:22:53 +0200

    wip, further SPI work

--- spi_litex/eos_spi.py
+++ spi_litex/eos_spi.py
@@ -5,10 +5,24 @@ from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.gen import *
 
+from litex.soc.cores.spi import SPISlave
+
+_io = [
+    ("user_led",   0, Pins("38"), IOStandard("LVCMOS33")), # blue
+    ("user_led",   1, Pins("39"), IOStandard("LVCMOS33")), # green
+    ("user_led",   2, Pins("34"), IOStandard("LVCMOS33")), # red
+    ("spi", 0,
+        Subsignal("clk", Pins("63")),
+        Subsignal("cs_n", Pins("59")),
+        Subsignal("mosi", Pins("64")),
+        Subsignal("miso", Pins("56")),
+        IOStandard("LVCMOS33")
+    )
+]
 
 class Platform(QuickLogicPlatform):
     def __init__(self, toolchain="f4pga"):
-        QuickLogicPlatform.__init__(self, "ql-eos-s3", [], toolchain=toolchain)
+        QuickLogicPlatform.__init__(self, "ql-eos-s3", _io, toolchain=toolchain)
 
 class _CRG(LiteXModule):
     def __init__(self):
@@ -24,6 +38,8 @@ class Soc(SoCCore):
         platform = Platform()
         args = {
             'with_uart': False,
+            'with_timer': False,
+            'with_ctrl': False,
             'cpu_type': 'eos_s3',
             'integrated_sram_size': 0
         }
@@ -36,8 +52,13 @@ class Soc(SoCCore):
             linker = True)
         )
 
+        spi_pads = platform.request("spi")
+        self.spi = SPISlave(spi_pads, 8)
+        platform.add_period_constraint(spi_pads.clk, 41.6) # 24 MHz
+
 def main():
-    builder = Builder(Soc())
+    soc = Soc()
+    builder = Builder(soc)
     builder.compile_software = False
     builder.build()
 
