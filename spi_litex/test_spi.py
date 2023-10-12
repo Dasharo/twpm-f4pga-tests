f12b1790de2920376bac80822efe44fc3baef026
Artur Kowalski <artur.kowalski@3mdeb.com>
2023-10-12 19:30:19 +0200

    simple SPI register

--- spi_litex/test_spi.py
+++ spi_litex/test_spi.py
@@ -6,5 +6,10 @@ from litex.soc.cores.spi import SPISlave
 
 class TestSpi(LiteXModule):
     def __init__(self, spi_pads):
+        self.register0 = Signal(32, reset=0x12345678)
+
         self.spi = SPISlave(spi_pads, 32)
-        self.spi.miso.eq(0x12345678)
+        self.sync += [
+            If(self.spi.irq, self.register0.eq(self.spi.mosi)),
+            self.spi.miso.eq(self.register0)
+        ]
